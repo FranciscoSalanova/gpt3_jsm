6c38e787e58fe3ff46a04f9df8d9eb3a917ef3dc
FranciscoSalanova <fsalanova.16@gmail.com>
2023-03-09 20:19:06 -0300

    refactoring WhatGPT3 container, working in Features container and styles

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import './whatGPT3.css'
 import { Feature } from '../../components'
 
+const featuresData = [
+  {
+    'title': 'Chatbots',
+    'text': 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+  },
+  {
+    'title': 'Knowledgebase',
+    'text': 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+  },
+  {
+    'title': 'Education',
+    'text': 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+  }
+]
+
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
@@ -20,18 +35,11 @@ const WhatGPT3 = () => {
         </p>
       </div>
       <div className='gpt3__whatgpt3-container'>
-        <Feature 
-          title="Chatbots"
-          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
-        />
-        <Feature 
-          title="Knowledgebase"
-          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
-        />
-        <Feature 
-          title="Education"
-          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
-        />   
+        {
+          featuresData.map((item, index) => (
+            <Feature title={item.title} text={item.text} key={item.title + index} />
+          ))
+        }
       </div>
     </div>
   )
